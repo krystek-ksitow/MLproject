440c5aaaae2423e83f55bec225782e2a6eee708b
DeepSeaWonder <a.goluchowski@gmail.com>
2025-01-25 18:56:10 +0100

    Create Getrequirements.py
    I have created a function that is supposed to import requirements from requirements.txt... it mostly works, user still needs to manually pip install requests and for some reason it does not import some things like pillow or scikit learn. Its still a good start tho. 
    
    Was it nesecarry? No, but its something others for sure wont have

---
+++ Functions/Getrequirements.py
@@ -0,0 +1,30 @@
+
+def getrequirements(url):
+
+    import requests
+    import importlib
+    import subprocess
+    import sys
+    import tempfile
+
+    rspns = requests.get(url)
+    rspns.raise_for_status()
+    requ =  rspns.text
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp:
+        tmp.write(requ)
+        tmpp = tmp.name
+    subprocess.check_call([sys.executable, "-m", "pip", "install", "-r", tmpp])
+    imported_modules = {}
+    for line in requ.splitlines():
+        line = line.strip()
+        if not line or line.startswith('#'):
+            continue
+        package_name = (line
+                        .split('==')[0])
+        try:
+            imported_modules[package_name] = importlib.import_module(package_name)
+        except ImportError:
+            print(f"Warning: Unable to import '{package_name}'")
+    return imported_modules
+
+#getrequirements('https://raw.githubusercontent.com/krystek-ksitow/MLproject/refs/heads/DSWBranch/requirementsTEST.txt')
