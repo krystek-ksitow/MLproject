dbe36339ba57610217fc78d06cc33278c3d68852
DeepSeaWonder <a.goluchowski@gmail.com>
2025-01-26 17:44:02 +0100

    Create corrmatrixfunction.py
    Made a function that generates the correlation matrix from chosen columns and saves it on local. Will make instruction for all the functions later. Will also figure out how we can automate figsize

---
+++ Functions/corrmatrixfunction.py
@@ -0,0 +1,21 @@
+def generate_corr_matrix(url, include_columns, output_file):
+    import pandas as pd
+    import seaborn as sns
+    import matplotlib.pyplot as plt
+
+    try:
+        dta = pd.read_csv(url)
+        sel_dta = dta[include_columns]
+        corr_matrix = sel_dta.corr()
+        plt.figure(figsize=(10, 8)) #Hmm... we gotta make it dynamic if we ever want bigger plots
+        sns.heatmap(corr_matrix, annot=True, cmap='coolwarm')
+        plt.title('Corr_Matrix')
+        plt.savefig(output_file, format='png')
+        plt.show()
+        plt.close()
+
+        print(f"Correlation matrix saved as {output_file}")
+    except KeyError as e:
+        print(f"Some columns were not found in the data: {e}")
+
+#generate_corr_matrix('https://raw.githubusercontent.com/krystek-ksitow/MLproject/refs/heads/DSWBranch/data/cleandatainonetable.csv', ['COPE_UseOfEmotionalSupport','COPE_BehavioralDisengagement','COPE_positiveReframing','COPE_Humor'], 'D:\The D\python projects\machinelearning\here.png')
