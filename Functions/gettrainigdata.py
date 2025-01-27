4fd067876b7f6a37e2d33ca20f129a9752766f35
DeepSeaWonder <a.goluchowski@gmail.com>
2025-01-27 13:26:59 +0100

    Update gettrainigdata.py
    uppdated

--- Functions/gettrainigdata.py
+++ Functions/gettrainigdata.py
@@ -1,11 +1,9 @@
-def gettrainingdata(csv_url, train_size=0.8, random_seed=None):
+def gettrainingdata(csv_url, train_size=0.8):
     import pandas as pd
     import numpy as np
     if not (0 < train_size < 1):
         raise ValueError("train_size must be between 0 and 1")
 
-    if random_seed is not None:
-        np.random.seed(random_seed)
     dta = pd.read_csv(csv_url)
     fturs = dta.iloc[:, :-1].values
     labls = dta.iloc[:, -1].values
@@ -20,5 +18,5 @@ def gettrainingdata(csv_url, train_size=0.8, random_seed=None):
 
     return x_train, x_test, y_train, y_test
 
-#x_train, x_test, y_train, y_test = gettrainingdata('https://raw.githubusercontent.com/krystek-ksitow/MLproject/refs/heads/DSWBranch/data/cleandatainonetable.csv', train_size=0.8, random_seed=None)
-#print("Training Deatures:\n", x_train)
+#x_train, x_test, y_train, y_test = gettrainingdata('https://raw.githubusercontent.com/krystek-ksitow/MLproject/refs/heads/DSWBranch/data/cleandatainonetable.csv', train_size=0.8)
+#print("Training Features:\n", x_train)
